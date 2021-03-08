a121ab61106b73c773747e479ae51baa5d02e300
kevin1610433 <slay1610@gmail.com>
2021-03-08 14:34:44 +0000

    Finding Faults through my mistakes completely missed off Mongo_dbname in heroku

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
-
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World ... again!"
+@app.route("/get_tasks")
+def get_tasks():
+    tasks = mongo.db.tasks.find()
+    return render_template("tasks.html", tasks=tasks)
+
+@app.route("/")
+def cocktails():
+    return "Cocktails at Home!"
 
 
 if __name__ == "__main__":
